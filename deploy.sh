c903b0530cd9f4263dcf96ed83b75f612eec641e
dukehug <yonglou.xu@adamson.edu.ph>
2026-05-05 17:26:06 +0800

    upadte notes: Tue May  5 05:26:06 PM CST 2026

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,27 @@
 #erro hanlding
 set -e 
 
+echo "Staring documentation build and deployment...."
+
+
 #mkdocs build
+echo "Building documentation...."
 mkdocs build --clean
 
 sleep 10
 
 #github
+echo "Pushing to github....."
 git add .
-git commit -m "upadte notes: $(date)"
-git push origin main
+
+# check if there are changes to comit 
+if ! git diff-index --quiet HEAD --; then
+    git commit -m "upadte notes: $(date)"
+    git push origin main
+    echo "Pushed to Github Successfully"
+else
+    echo "No change to comit"
+fi
 
 sleep 10
 
